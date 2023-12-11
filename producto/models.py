5a005b1548f12fabf87161dc9d7d08ab6e8158ec
Lenzx <leo_1998@live.cl>
2023-12-11 13:43:05 -0300

    segundo commit

--- producto/models.py
+++ producto/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Categoria(models.Model):
+    categoria_id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+
+class Producto(models.Model):
+    producto_id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    descripcion_corta = models.CharField(max_length=500)
+    descripcion_larga = models.CharField(max_length=1000)
+    valor = models.IntegerField()
+    necesita_receta = models.BooleanField(default=False)
+    imagen = CloudinaryField('image')
+    categoria = models.ManyToManyField(Categoria,through='Producto_categoria',related_name='productos')
+
+class Producto_categoria(models.Model):
+    producto_categoria_id = models.AutoField(primary_key=True)
+    categoria_id = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    producto_id = models.ForeignKey(Producto, on_delete=models.CASCADE)
